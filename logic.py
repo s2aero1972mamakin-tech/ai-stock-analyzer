5fdf68f2e1f33ad05f0e78f56275afec52f40740
s2aero1972mamakin-tech <s2.aero1972.mamakin@gmail.com>
2026-03-02 13:21:30 +0900

    Update logic.py

--- logic.py
+++ logic.py
@@ -28,6 +28,7 @@ from typing import Dict, List, Optional, Tuple
 import numpy as np
 import pandas as pd
 import requests
+from concurrent.futures import ThreadPoolExecutor
 
 
 # -----------------------------
